36c10229abb5e1279e058237bd05de151a959b5a
Tyrone <tschoeman7@gmail.com>
2024-02-27 19:20:16 +0200

    added user prompt where userinput number equals total num of squares per side eg if user enters 10 will show 10 * 10

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
+let userInput = prompt("enter how many squares you want per side: ");
+
 let grid = document.querySelector(".grid");
 
-for (let i = 0; i < 256; i++) {
+let heightAndWidth = 100 / userInput;
+let totalNumSquares = userInput * userInput;
+
+for (let i = 1; i <= totalNumSquares; i++) {
   let gridItem = document.createElement("div");
   gridItem.setAttribute("class", "grid-item");
   gridItem.textContent = i;
   grid.appendChild(gridItem);
+  gridItem.style.width = heightAndWidth + "%";
+  gridItem.style.height = heightAndWidth + "vh";
 }
+// Change the background color
+
+// Change the font size
+// myElement.style.fontSize = "16px";
