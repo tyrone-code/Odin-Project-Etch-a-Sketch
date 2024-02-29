c0d48a68430a4e7076275b9e869f782773ef0322
Tyrone <tschoeman7@gmail.com>
2024-02-29 21:01:20 +0200

    added reset button, made the grid borders thinner

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 let grid = document.querySelector(".grid");
 let userInput = "";
-// for (let i = 1; i <= totalNumSquares; i++) {}
-
+let resetButtonDiv = document.querySelector(".resetButtonDiv");
 let makeGrid = function () {
+  let resetButton = document.createElement("button");
+
+  resetButton.setAttribute("class", "reset-grid");
+  resetButton.innerText = "RESET GRID";
+  resetButtonDiv.appendChild(resetButton);
   userInput = prompt("enter how many squares you want per side (max 100): ");
   let heightAndWidth = 100 / userInput;
   let totalNumSquares = userInput * userInput;
   let i = 0;
+
   while (userInput <= 100) {
     i++;
     if (i > totalNumSquares) {
@@ -14,17 +19,23 @@ let makeGrid = function () {
     }
     let gridItem = document.createElement("div");
     gridItem.setAttribute("class", "grid-item");
-    grid.appendChild(gridItem);
     gridItem.style.width = heightAndWidth + "%";
     gridItem.style.height = heightAndWidth + "vh";
-    if (userInput >= 40) {
-      gridItem.style.width = 2 + "%";
-      gridItem.style.height = 3 + "vh";
-    }
+    grid.appendChild(gridItem);
   }
   if (userInput > 100) {
     makeGrid();
   }
+  resetButton.addEventListener("click", () => {
+    const response = confirm("Are you sure you want to reset the grid?");
+
+    if (response) {
+      location.reload();
+    } else {
+      console.log(null);
+    }
+  });
 };
+// the JavaScript file
 
 makeGrid();
